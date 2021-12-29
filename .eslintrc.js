9d5de82755bd6164c95bacc8f9889aad8c1d71c1
Grégory <gc.webmaster69@gmail.com>
2021-12-29 11:34:44 +0100

    add computed to sort

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,7 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "semicolon": [true, "never", "ignore-interfaces"]
+    "semicolon": [0, "never"],
+    "prettier/prettier": ["error", { endOfLine: "auto" }],
   },
 };
--- src/components/JobsList.vue
+++ src/components/JobsList.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="job-list">
+    <p>Ordered by {{ order }}</p>
     <ul>
-      <li v-for="job in jobs" :key="job.id">
+      <li v-for="job in orderedJobs" :key="job.id">
         <h2>{{ job.title }} in {{ job.location }}</h2>
         <div class="salary">
           <p>{{ job.salary }} rupees</p>
         </div>
-        <div class="descripti">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, omnis eveniet quo, soluta similique nihil repellendus deserunt consequatur autem dolorum illo dolor voluptates obcaecati. Necessitatibus laudantium
-          magnam debitis nisi modi.
+        <div class="description">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, omnis
+          eveniet quo, soluta similique nihil repellendus deserunt consequatur
+          autem dolorum illo dolor voluptates obcaecati. Necessitatibus
+          laudantium magnam debitis nisi modi.
         </div>
       </li>
     </ul>
@@ -16,8 +19,9 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from "vue";
+import { computed, defineComponent, PropType } from "vue";
 import Job from "@/types/Job";
+import OrderTerm from "@/types/OrderTerm";
 
 export default defineComponent({
   props: {
@@ -25,6 +29,19 @@ export default defineComponent({
       required: true,
       type: Array as PropType<Job[]>,
     },
+    order: {
+      required: true,
+      type: String as PropType<OrderTerm>,
+    },
+  },
+  setup(props) {
+    const orderedJobs = computed(() => {
+      return [...props.jobs].sort((a: Job, b: Job) => {
+        return a[props.order] > b[props.order] ? 1 : -1;
+      });
+    });
+
+    return { orderedJobs };
   },
 });
 </script>
